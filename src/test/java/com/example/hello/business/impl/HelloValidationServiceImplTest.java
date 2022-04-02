7f803c00137934c445c5d98d8659946cb2ed54a6
swdev.smz <swdev.smz@gmail.com>
2022-04-03 00:22:58 +0900

    SpringNativeがMockitoに対応していなため、コメントアウト

--- src/test/java/com/example/hello/business/impl/HelloValidationServiceImplTest.java
+++ src/test/java/com/example/hello/business/impl/HelloValidationServiceImplTest.java
@@ -5,11 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 public class HelloValidationServiceImplTest {
+
+    // @InjectMocks
+    // HelloValidationServiceImpl target;
+    // @Mock
+    // MessageSource messageSource;
+
     @Test
     void testExecute() {
+        // doReturn("aaa").when(messageSource).getMessage(Mockito.any(), Mockito.any(),
+        // Mockito.any());
         assertTrue(true);
     }
 }
